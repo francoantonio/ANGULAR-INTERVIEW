9f7bfabe884b548db352632b2fd68d116b17f15f
Antonio Martin Franco <antoniofranco.dev@gmail.com>
2021-12-19 10:02:30 -0300

    add page PostDetail

---
+++ src/app/Pages/post-detail/post-detail.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from 'src/app/postsInterface';
+import { PostsService } from 'src/app/services/posts.service';
+
+@Component({
+  selector: 'app-post-detail',
+  templateUrl: './post-detail.component.html',
+  styleUrls: ['./post-detail.component.scss'],
+})
+export class PostDetailComponent implements OnInit {
+  post!: Post;
+  currentId!: number;
+  dateLastComment!: string;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private postsService: PostsService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      this.currentId = params['id'];
+      console.log(this.currentId);
+      this.postsService.getPost(params['id']).subscribe(
+        (post) => (this.post = post),
+        () => {
+          this.router.navigate(['/404/' + this.currentId]);
+        }
+      );
+    });
+  }
+  ngDoCheck(): void {}
+  capturarData(mensaje: string) {
+    this.dateLastComment = mensaje;
+  }
+}
