2c20c40ad3cf5304b2a1f7f3f72393e49c6487e1
Antonio Martin Franco <antoniofranco.dev@gmail.com>
2021-12-19 08:50:03 -0300

    Add routing and environment variable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './Pages/not-found/not-found.component';
+import { PostDetailComponent } from './Pages/post-detail/post-detail.component';
+import { PostListComponent } from './Pages/post-list/post-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'posts' },
+  { path: 'posts', component: PostListComponent },
+  { path: 'post/:id', component: PostDetailComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '404/:id', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
